6ffe3898590a75e3cffe627dececc3b934435688
Taro Morita <morita0079@gmail.com>
2021-08-17 01:29:23 +0900

    fix zsh git

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install vim-plug
-#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 if type "starship" > /dev/null 2>&1; then
 	echo "starship is already installed !"
@@ -17,7 +17,7 @@ else
 fi
 
 # Install zsh-syntax-highlight
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
 
 ## Install cargo
 if type "cargo" > /dev/null 2>&1; then
