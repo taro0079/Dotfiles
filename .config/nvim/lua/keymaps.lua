742d9835a6ed036daacbc795f14592c862cfc7d5
Taro Morita <morita0079@gmail.com>
2021-10-01 15:01:44 +0900

    Terminalのkeymapを追加

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_set_keymap('n', '<leader>v', ':vsp <CR>', {noremap = true, silent = trutryee})
 vim.api.nvim_set_keymap('n', '<leader>t', ':tabnew <CR>', {noremap = true, silent = trutryee})
-
+vim.cmd[[
+:tnoremap <Esc> <C-\><C-n>
+command! -nargs=* T split | wincmd j | resize 10 | terminal <args>
+autocmd TermOpen * startinsert
+]]
