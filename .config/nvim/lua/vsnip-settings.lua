a3e73921b0eb9f357081f577ae8041c6fefc98f8
Taro Morita <morita0079@gmail.com>
2021-11-26 20:40:29 +0900

    fix vsnips

--- .config/nvim/lua/vsnip-settings.lua
+++ .config/nvim/lua/vsnip-settings.lua
@@ -13,10 +13,3 @@ vim.cmd[[imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'    
 vim.cmd[[smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>']]
 vim.cmd[[imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>']]
 vim.cmd[[smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>']]
-
--- Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
--- See https://github.com/hrsh7th/vim-vsnip/pull/50
-vim.cmd[[nmap        s   <Plug>(vsnip-select-text)]]
-vim.cmd[[xmap        s   <Plug>(vsnip-select-text)]]
-vim.cmd[[nmap        S   <Plug>(vsnip-cut-text)]]
-vim.cmd[[xmap        S   <Plug>(vsnip-cut-text)]]
