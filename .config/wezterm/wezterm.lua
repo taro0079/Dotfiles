70dcae8536d9fcd5665e9171f1e621f4caa1bec5
Taro Morita <morita0079@gmail.com>
2022-05-25 02:46:08 +0900

    about wezterm and tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,19 +3,17 @@ local wezterm = require 'wezterm';
 return {
 	-- font = wezterm.font("JetBrains Mono", {weight="Bold", italic=true}),
 	font = wezterm.font_with_fallback({
-		{family = "JetBrainsMono Nerd Font", weight="Medium", italic=false},
-		{family = "JetBrainsMono Nerd Font", weight="Medium", italic=false},
-		{family = "JetBrainsMono Nerd Font", weight="Medium", italic=false},
-		{family = "VictorMono Nerd Font Mono", weight="Medium", italic=true},
-		{family = "HackGenNerd", weight="Medium", italic=true},
-	
-		
+		{ family = "JetBrainsMono Nerd Font", weight = "Medium", italic = false },
+		{ family = "JetBrainsMono Nerd Font", weight = "Medium", italic = false },
+		{ family = "JetBrainsMono Nerd Font", weight = "Medium", italic = false },
+		{ family = "VictorMono Nerd Font Mono", weight = "Medium", italic = true },
+		{ family = "HackGenNerd", weight = "Medium", italic = true },
+
+
 	}),
 	font_size = 15,
-	color_scheme = "nord",
+	color_scheme = "Catppuccin",
 	send_composed_key_when_right_alt_is_pressed = false,
 	enable_tab_bar = false,
 
 }
-
-
