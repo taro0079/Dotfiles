7e86bbe01df3d3db5404e57a79be0c055216d619
Taro Morita <morita0079@gmail.com>
2022-06-11 19:50:23 +0900

    fix diffview setting

--- .config/nvim/lua/diffview-setting.lua
+++ .config/nvim/lua/diffview-setting.lua
@@ -34,13 +34,13 @@ require'diffview'.setup {
 
     log_options = {
 
-      max_count = 256,      -- Limit the number of commits
-      follow = false,       -- Follow renames (only for single file)
-      all = false,          -- Include all refs under 'refs/' including HEAD
-      merges = false,       -- List only merge commits
-      no_merges = false,    -- List no merge commits
+      -- max_count = 256,      -- Limit the number of commits
+      -- follow = false,       -- Follow renames (only for single file)
+      -- all = false,          -- Include all refs under 'refs/' including HEAD
+      -- merges = false,       -- List only merge commits
+      -- no_merges = false,    -- List no merge commits
 
-      reverse = false,      -- List commits in reverse order
+      -- reverse = false,      -- List commits in reverse order
 
     },
   },
