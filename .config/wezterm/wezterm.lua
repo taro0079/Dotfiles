62fc12c5db38a467a493e0cee79115da6013621d
Taro Morita <morita0079@gmail.com>
2022-07-07 20:45:08 +0900

    weztermのフォント設定を変更

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,16 +3,13 @@ local wezterm = require 'wezterm';
 return {
 	-- font = wezterm.font("JetBrains Mono", {weight="Bold", italic=true}),
 	font = wezterm.font_with_fallback({
-		{ family = "Cascadia Code PL", weight = "Regular", italic = false },
-		{ family = "Cascadia Code PL", weight = "Regular", italic = true },
-		{ family = "CaskaydiaCove Nerd Font", weight = "Regular", italic = false },
-		{ family = "JetBrainsMono Nerd Font", weight = "Regular", italic = false },
-		{ family = "VictorMono Nerd Font Mono", weight = "Regular", italic = true },
+		{ family = "FantasqueSansMono Nerd Font", weight = "Regular", italic = false },
+		{ family = "FantasqueSansMono Nerd Font", weight = "Regular", italic = true },
 		{ family = "HackGenNerd", weight = "Regular", italic = true },
 
 
 	}),
-	font_size = 16,
+	font_size = 17,
 	color_scheme = "Dracula",
 	send_composed_key_when_right_alt_is_pressed = false,
 	enable_tab_bar = false,
