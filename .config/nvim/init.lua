67259e6cd8485d8f7acd157f745c1921782ae955
Taro Morita <morita0079@gmail.com>
2022-08-23 16:52:32 +0900

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 require('options')
 require('keys')
 require('plugins')
-vim.cmd("let g:everforest_background = 'soft'")
+vim.cmd("let g:everforest_background = 'medium'")
 vim.cmd('colorscheme everforest')
 require('config')
 require('gui-settings')
