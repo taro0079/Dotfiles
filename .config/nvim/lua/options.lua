02a50ecf849d58f828f9d2d10f39fb18366b6d5e
Taro Morita <morita0079@gmail.com>
2022-09-06 03:09:03 +0900

    comma as leader

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,7 +1,8 @@
-vim.wo.number=true
-vim.o.smarttab=true
-vim.o.cursorline=true
-vim.o.pumblend=20
+vim.g.maplocalleader = ','
+vim.wo.number = true
+vim.o.smarttab = true
+vim.o.cursorline = true
+vim.o.pumblend = 20
 vim.o.clipboard = "unnamed,unnamedplus"
 vim.o.completeopt = "menuone,noinsert,noselect"
 vim.o.tabstop = 2
