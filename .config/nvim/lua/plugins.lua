692b55466fa28c28ee17a1c2a32bff8190448aba
Taro Morita <morita0079@gmail.com>
2022-09-12 15:03:11 +0900

    add firevim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -124,5 +124,9 @@ return require('packer').startup(function(use)
 	use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
 		setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 	use { 'previm/previm' }
+	use {
+		'glacambre/firenvim',
+		run = function() vim.fn['firenvim#install'](0) end
+	}
 
 end)
