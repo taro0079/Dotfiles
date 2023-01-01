4107d3b68847895c064cd89971e3d6dc6059c5b0
Morita Taro <38848223+taro0079@users.noreply.github.com>
2023-01-02 00:20:02 +0900

    change nvim-tree settings

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -5,6 +5,10 @@ vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
   view = {
     adaptive_size = true,
     mappings = {
@@ -17,7 +21,6 @@ require("nvim-tree").setup({
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false
   },
 })
-
