52c00642ef22ea545fd02d88c7fe3418f89765aa
Taro Morita <morita0079@gmail.com>
2023-02-15 19:44:47 +0900

    fix lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,8 @@ local lualine = require('lualine')
 lualine.setup{
 				options = {
 								theme = "auto",
-								section_separators = { left = '', right = '' },
-								component_separators = { left = ' ', right = ' ' }
+								section_separators = { left = '', right = '' },
+								component_separators = { left = '', right = '' }
 				},
 				sections = {
 								lualine_a = {
