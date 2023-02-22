226f8e860f4fbe78e0601a28a806b712f2d3d12d
Morita Taro <38848223+taro0079@users.noreply.github.com>
2023-02-23 08:42:04 +0900

    prettier cli setting

--- .config/nvim/lua/plugins/prettier.lua
+++ .config/nvim/lua/plugins/prettier.lua
@@ -13,5 +13,9 @@ prettier.setup({
       return true
     end,
     timeout = 5000,
-  }
+  },
+  cli_options = {
+    -- https://prettier.io/docs/en/cli.html#--config-precedence
+    config_precedence = "prefer-file", -- or "cli-override" or "file-override"
+  },
 })
