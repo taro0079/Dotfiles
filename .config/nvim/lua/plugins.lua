5ee38997a5063d32a8c507dceb9d43e5f7f31f65
Morita Taro <38848223+taro0079@users.noreply.github.com>
2023-03-08 15:30:19 +0900

    lazy.nvim対応。遅延ロード

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,197 +1,282 @@
-return require("packer").startup({
-  function(use)
-    use "whatyouhide/vim-gotham"
-    use 'liuchengxu/vista.vim'
-    use "soramugi/auto-ctags.vim"
-    use {
-      "folke/trouble.nvim",
-      requires = "nvim-tree/nvim-web-devicons",
-      config = function()
-        require("trouble").setup {
-
-        }
-      end
-    }
-    use {
-      "utilyre/barbecue.nvim",
-      requires = {
-        "SmiteshP/nvim-navic"
+--     use({
+--       "fatih/vim-go"
+--     })
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--     -- devicon
+--     use { 'nvim-tree/nvim-web-devicons' }
+--     -- cmp
+--     use { 'hrsh7th/nvim-cmp', config = function() require 'plugins.cmp' end }
+--     use 'hrsh7th/cmp-calc'
+--     use 'hrsh7th/cmp-omni'
+--     use 'hrsh7th/cmp-copilot'
+--     use { 'hrsh7th/cmp-nvim-lsp-signature-help' }
+--     use { 'quangnguyen30192/cmp-nvim-ultisnips' }
+--     use { 'hrsh7th/cmp-nvim-lsp-document-symbol' }
+--     use { 'hrsh7th/cmp-emoji' }
+--     use { 'uga-rosa/cmp-dictionary', config = function() require 'plugins.dictionary' end }
+--     use 'hrsh7th/cmp-nvim-lsp'
+--     use 'hrsh7th/cmp-buffer'
+--     use 'hrsh7th/cmp-path'
+--     use 'hrsh7th/cmp-cmdline'
+--     use 'f3fora/cmp-spell'
+--     use { 'aca/marp.nvim' }
+--   end,
+--   config = { display = { open_cmd = "leftabove 75vnew \\[packer\\]" }, max_jobs = 10 },
+-- })
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  "folke/which-key.nvim",
+"soramugi/auto-ctags.vim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+ {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+          require("plugins.tree-sitter")
+        end,
       },
-      config = function() require("barbecue").setup() end
-    }
-    use({
-      "numToStr/Navigator.nvim",
-      config = function() require("plugins.navigator") end
-    })
-    use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons', config = function()
-      require(
-        "plugins.bufferline")
-    end }
-
-    use({
-      "hrsh7th/nvim-insx",
-      config = function() require('insx.preset.standard').setup() end
-    })
-    use({
-      "fatih/vim-go"
-    })
-    use({
-      "phaazon/hop.nvim",
-      branch = "v2", -- optional but strongly recommended
-      config = function()
-        -- you can configure Hop the way you like here; see :h hop-config
-        require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-      end,
-    })
-
-    -- Coc
-    use { 'lervag/vimtex' }
-    use { 'koenverburg/peepsight.nvim',
-      config = function()
-        require("plugins.peepsight")
-      end
-    }
-
-
-    -- use(
-    --   { "projekt0n/github-nvim-theme",
-    --     config = function()
-    --       require("plugins.github-nvim-theme")
-    --     end
-    --   }
-    -- )
-    use { "catppuccin/nvim", as = "catppuccin" }
-
-
-    use("github/copilot.vim")
-    use({
-      "ellisonleao/glow.nvim",
-      config = function()
-        require("plugins.glow")
-      end,
-    })
-    use({ "wbthomason/packer.nvim" })
-    -- colorscheme
-    use({
-      "folke/tokyonight.nvim",
-      config = function()
-        require("plugins.tokyonight")
-      end,
-    })
-    use({
-      "EdenEast/nightfox.nvim",
-      config = function()
-        require("plugins.nightfox")
-      end,
-    })
-    use({
-      "hoob3rt/lualine.nvim",
-      -- after = "github-nvim-theme",
-      config = function()
-        require("plugins.lualine")
-      end,
-    })
-
-    -- fuzzy finder
-    use({
-      "nvim-telescope/telescope.nvim",
-      requires = { { "nvim-lua/plenary.nvim" } },
-      config = function()
-        require("plugins.telescope")
-      end,
-    })
-
-    -- treesitter
-    use({ "windwp/nvim-ts-autotag", config = function() require "plugins.nvim-ts-autotag" end })
-    use({
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
-      config = function()
-        require("plugins.tree-sitter")
-      end,
-    })
-    use({
+{
       'nvim-treesitter/nvim-treesitter-context',
       config = function()
         require("plugins.treesitter-context")
       end,
-    })
-    use({ "RRethy/nvim-treesitter-endwise" })
+      dependencies ={
+        "nvim-treesitter/nvim-treesitter",
 
-    -- languages
-    use({ "tpope/vim-rails", ft = { "ruby" } })
-    use({ "rust-lang/rust.vim", ft = { "rs" } })
+      }
+    },
+    { "RRethy/nvim-treesitter-endwise",
 
-    -- tools
-    use({
-      "vim-skk/skkeleton",
-      requires = { "vim-denops/denops.vim" },
-      config = function()
-        require("plugins.skkeleton")
-      end,
-    })
-    use({
-      "delphinus/skkeleton_indicator.nvim",
-      config = function()
-        require("skkeleton_indicator").setup({})
-      end,
-    })
-    use({
-      "lukas-reineke/indent-blankline.nvim",
-      config = function()
-        require("plugins.indent-blankline")
-      end,
-    }) -- show indent
-    -- use({ "haya14busa/vim-edgemotion" })
-    -- use({ "t9md/vim-quickhl" }) -- for highlighting
-    use({ "tpope/vim-surround" })
-    use({ "junegunn/vim-easy-align" })
-    use({
+      dependencies ={
+        "nvim-treesitter/nvim-treesitter",
+
+      }
+  },
+  "folke/neodev.nvim",
+  { 'liuchengxu/vista.vim', cmd="Vista" },
+  { 'onsails/lspkind-nvim', dependencies="hrsh7th/nvim-cmp" },
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  {
+    "cshuaimin/ssr.nvim",
+    -- init is always executed during startup, but doesn't load the plugin yet.
+    init = function()
+      vim.keymap.set({ "n", "x" }, "<leader>cR", function()
+        -- this require will automatically load the plugin
+        require("ssr").open()
+      end, { desc = "Structural Replace" })
+    end,
+  },
+  {
+    "dstein64/vim-startuptime",
+    -- lazy-load on a command
+    cmd = "StartupTime",
+  },
+  {
+    "github/copilot.vim",
+    event="InsertEnter"
+  },
+
+
+  {
+    "hrsh7th/nvim-cmp",
+    -- load cmp on InsertEnter
+    event = "InsertEnter",
+    -- these dependencies will only be loaded when cmp loads
+    -- dependencies are always lazy-loaded unless specified otherwise
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      'hrsh7th/cmp-calc',
+      'hrsh7th/cmp-omni',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'quangnguyen30192/cmp-nvim-ultisnips',
+      'hrsh7th/cmp-nvim-lsp-document-symbol',
+      'hrsh7th/cmp-emoji',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'f3fora/cmp-spell',
+      'hrsh7th/cmp-copilot'
+      -- 'uga-rosa/cmp-dictionary', config = function() require 'plugins.dictionary' end } #TODO
+    },
+    config = function()
+      require 'plugins.cmp'
+    end,
+  },
+ {
+       "utilyre/barbecue.nvim",
+       dependencies = {
+         "SmiteshP/nvim-navic"
+       },
+       config = function() require("barbecue").setup() end
+     },
+
+{
+       "folke/trouble.nvim",
+       dependencies = "nvim-tree/nvim-web-devicons",
+       config = function()
+         require("trouble").setup {
+
+         }
+       end
+     },
+     {
+       "numToStr/Navigator.nvim",
+       config = function() require("plugins.navigator") end
+     },
+
+{ 
+  'akinsho/bufferline.nvim', 
+  dependencies = 'nvim-tree/nvim-web-devicons', 
+  config = function()
+    require("plugins.bufferline")
+  end
+},
+
+{
+       "phaazon/hop.nvim",
+       config = function()
+         require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+       end
+     },
+     { 'koenverburg/peepsight.nvim',
+       config = function()
+         require("plugins.peepsight")
+       end,
+       dependencies='nvim-treesitter/nvim-treesitter'
+       
+     },
+     
+     {
+       "hoob3rt/lualine.nvim",
+       config = function()
+         require("plugins.lualine")
+       end,
+     },
+{
+  "nvim-telescope/telescope.nvim",
+  -- lazy=true,
+  dependencies = {  "nvim-lua/plenary.nvim"  },
+  config = function()
+    require("plugins.telescope")
+  end,
+},
+{ "stevearc/dressing.nvim", event = "VeryLazy" },
+
+{ "windwp/nvim-ts-autotag", 
+config = function() require "plugins.nvim-ts-autotag" end,
+event="InsertEnter",
+ft={"html", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue", "markdown"}
+},
+{
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require("plugins.toggleterm")
+  end,
+},
+{
       "numToStr/Comment.nvim",
       config = function()
         require("Comment").setup()
       end,
-    }) -- plugin for comment out
-    use({
-      "akinsho/toggleterm.nvim",
-      config = function()
-        require("plugins.toggleterm")
-      end,
-    })
-    use({
+    },
+    {
       "windwp/nvim-autopairs",
       config = function()
         require("nvim-autopairs").setup()
       end,
-    })
-    use({ "tpope/vim-repeat" })
-    use({
+      event="InsertEnter",
+    },
+    { "tpope/vim-repeat" },
+{ "tpope/vim-rails", ft = { "ruby" } },
+{ "rust-lang/rust.vim", ft = { "rs" } },
+
+{
+  "nkakouros-original/numbers.nvim",
+  config = function()
+    require("numbers").setup()
+  end,
+},
+    {
+      "vim-skk/skkeleton",
+      dependencies = { "vim-denops/denops.vim" },
+      event="InsertEnter",
+      config = function()
+        require("plugins.skkeleton")
+      end,
+    },
+    {
+      "delphinus/skkeleton_indicator.nvim",
+      config = function()
+        require("skkeleton_indicator").setup({})
+      end,
+    },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      config = function()
+        require("plugins.indent-blankline")
+      end,
+    },
+    { "haya14busa/vim-edgemotion" },
+    { "t9md/vim-quickhl" },
+    { "tpope/vim-surround", event="InsertEnter" },
+    { "junegunn/vim-easy-align" },
+    {
       "petertriho/nvim-scrollbar",
       config = function()
         require("scrollbar").setup({})
       end,
-    })
+    },
     -- use { 'ggandor/lightspeed.nvim' }
-    use({
+    {
       "folke/todo-comments.nvim",
-      requires = "nvim-lua/plenary.nvim",
+      dependencies = "nvim-lua/plenary.nvim",
       config = function()
         require("todo-comments").setup({})
       end,
-    })
-    use({
+    },
+    {
       "ziontee113/icon-picker.nvim",
       config = function()
         require("icon-picker")
       end,
-    })
-
-    use({
+    },
+    {
       'nvim-tree/nvim-tree.lua',
       config = function()
         require("plugins.nvim-tree")
       end
-    })
+    },
 
     -- fern file explorer
     -- use({ "lambdalisue/fern.vim" })
@@ -212,78 +297,50 @@ return require("packer").startup({
     -- 		require("plugins.vgit")
     -- 	end,
     -- })
-    use({
+    {
       'lewis6991/gitsigns.nvim',
       config = function()
         require("plugins.gitsign")
       end
-    })
-    use({ "kdheepak/lazygit.nvim" })
-    use({ "lambdalisue/gina.vim" })
-    use({ "lambdalisue/gin.vim" })
-    use({
+    },
+    { "kdheepak/lazygit.nvim", cmd="Lazygit" },
+    { "lambdalisue/gina.vim", cmd="Gina"},
+    { "lambdalisue/gin.vim", cmd="Gin" },
+    {
       "akinsho/git-conflict.nvim",
-      tag = "*",
       config = function()
         require("git-conflict").setup()
       end,
-    })
-    use { 'TimUntersberger/neogit' }
+    },
+    { 'TimUntersberger/neogit',
+    cmd = "Neogit"
+  },
 
-    use({
+    {
       "j-hui/fidget.nvim",
       config = function()
         require("fidget").setup({})
       end,
-    })
+    },
     -- use({ "fatih/vim-go", opt = true, ft = { "go" } })
 
     -- snippets
-    use({ "SirVer/ultisnips" })
-    use({ "honza/vim-snippets" })
-    use({
-      "nkakouros-original/numbers.nvim",
-      config = function()
-        require("numbers").setup()
-      end,
-    })
-    use({ "previm/previm" })
+    { "SirVer/ultisnips", event="InsertEnter" },
+    { "honza/vim-snippets", event="InsertEnter"},
+    { "previm/previm" },
 
     -- nvim-lsp
-    use { 'ray-x/lsp_signature.nvim', config = function() require "lsp_signature".setup() end }
-    use 'onsails/lspkind-nvim'
-    use { 'williamboman/mason.nvim' }
-    use { 'williamboman/mason-lspconfig.nvim' }
-    use { 'junegunn/vim-emoji' }
-    use { 'folke/lsp-colors.nvim' }
-    use { 'RRethy/vim-illuminate' }
-    use { 'jose-elias-alvarez/null-ls.nvim', config = function() require 'plugins.null-ls' end,
-      requires = { "nvim-lua/plenary.nvim" } }
-    use { 'neovim/nvim-lspconfig', config = function() require 'plugins.lsp' end }
-    use { 'MunifTanjim/prettier.nvim', config = function() require 'plugins.prettier' end }
+    { 'ray-x/lsp_signature.nvim', config = function() require "lsp_signature".setup() end },
+    { 'williamboman/mason.nvim', dependencies={ 'williamboman/mason-lspconfig.nvim' }},
+    { 'junegunn/vim-emoji' },
+    { 'folke/lsp-colors.nvim' },
+    { 'RRethy/vim-illuminate' },
+    { 'jose-elias-alvarez/null-ls.nvim', config = function() require 'plugins.null-ls' end,
+      dependencies = { "nvim-lua/plenary.nvim" } },
+    { 'neovim/nvim-lspconfig', config = function() require 'plugins.lsp' end },
+    { 'MunifTanjim/prettier.nvim', config = function() require 'plugins.prettier' end },
+    { 'rinx/cmp-skkeleton', dependencies = { 'nvim-cmp', 'skkeleton' }, event="InsertEnter" }
 
 
 
-
-    -- devicon
-    use { 'nvim-tree/nvim-web-devicons' }
-    -- cmp
-    use { 'hrsh7th/nvim-cmp', config = function() require 'plugins.cmp' end }
-    use 'hrsh7th/cmp-calc'
-    use 'hrsh7th/cmp-omni'
-    use 'hrsh7th/cmp-copilot'
-    use { 'hrsh7th/cmp-nvim-lsp-signature-help' }
-    use { 'quangnguyen30192/cmp-nvim-ultisnips' }
-    use { 'hrsh7th/cmp-nvim-lsp-document-symbol' }
-    use { 'hrsh7th/cmp-emoji' }
-    use { 'uga-rosa/cmp-dictionary', config = function() require 'plugins.dictionary' end }
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'f3fora/cmp-spell'
-    use { 'rinx/cmp-skkeleton', after = { 'nvim-cmp', 'skkeleton' } }
-    use { 'aca/marp.nvim' }
-  end,
-  config = { display = { open_cmd = "leftabove 75vnew \\[packer\\]" }, max_jobs = 10 },
 })
