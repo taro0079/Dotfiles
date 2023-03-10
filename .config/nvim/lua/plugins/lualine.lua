1bf98dbca31ed30edf2db966424c58e7a4acecd1
Morita Taro <38848223+taro0079@users.noreply.github.com>
2023-03-10 14:25:42 +0900

    fix lualine setting for nightfox

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 local lualine = require('lualine')
 lualine.setup {
   options = {
-    theme = "tokyonight",
+    theme = "auto",
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' }
   },
