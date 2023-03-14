513c5c0a033496a73b35f834570c4cd60964c5a4
Morita Taro <38848223+taro0079@users.noreply.github.com>
2023-03-15 00:30:39 +0900

    delete hop keymapping

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -38,18 +38,18 @@ api.nvim_set_keymap('n', "<C-n>", ":NvimTreeToggle<cr>", { noremap = true, silen
 api.nvim_set_keymap('n', "<leader>sd", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics() <cr>",
 	{ noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('', 'f',
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
-	, {})
-vim.api.nvim_set_keymap('', 'F',
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
-	, {})
-vim.api.nvim_set_keymap('', 't',
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>"
-	, {})
-vim.api.nvim_set_keymap('', 'T',
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>"
-	, {})
+-- vim.api.nvim_set_keymap('', 'f',
+-- 	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
+-- 	, {})
+-- vim.api.nvim_set_keymap('', 'F',
+-- 	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
+-- 	, {})
+-- vim.api.nvim_set_keymap('', 't',
+-- 	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>"
+-- 	, {})
+-- vim.api.nvim_set_keymap('', 'T',
+-- 	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>"
+-- 	, {})
 vim.api.nvim_set_keymap('', '<leader><leader>w',
 	"<cmd> HopVertical <cr>"
 	, {})
