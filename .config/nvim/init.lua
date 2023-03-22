7b61134b87aa97ece9a661248a8db3918685a640
Taro Morita <morita0079@gmail.com>
2023-03-23 00:50:02 +0900

    setting for vscode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,12 @@
+local is_vscode = vim.fn.exists('g:vscode') == 1
+
+if is_vscode then
+  require('vscode-plugins')
+  require('vscode-keymaps')
+else
 require('options')
 require('plugins')
 require('keys')
 require('config')
 require('gui-settings')
+end
