c0e0ea9c7df558976ddbae9b42bf409a5322c662
Taro Morita <morita0079@gmail.com>
2023-04-20 01:10:39 +0900

    fix colorscheme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -139,6 +139,10 @@ require("lazy").setup({
     lazy=false,
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background=true
+      })
       vim.cmd([[colorscheme catppuccin]])
     end
 
