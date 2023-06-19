ff6bfa9fbc6f97d3e652b19bf74b1cb3b4026182
Taro Morita <morita0079@gmail.com>
2023-06-19 20:13:01 +0900

    fix

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -111,6 +111,29 @@ require("lazy").setup({
     config = true
   },
   {
+    'johnfrankmorgan/whitespace.nvim',
+    config = function()
+      require('whitespace-nvim').setup({
+        -- configuration options and their defaults
+
+        -- `highlight` configures which highlight is used to display
+        -- trailing whitespace
+        highlight = 'DiffDelete',
+
+        -- `ignored_filetypes` configures which filetypes to ignore when
+        -- displaying trailing whitespace
+        ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
+
+        -- `ignore_terminal` configures whether to ignore terminal buffers
+        ignore_terminal = true,
+      })
+
+      -- remove trailing whitespace with a keybinding
+      vim.keymap.set('n', '<Leader>t', require('whitespace-nvim').trim)
+    end
+  },
+
+  {
     'nvim-treesitter/nvim-treesitter-context',
     config = function()
       require("plugins.treesitter-context")
@@ -121,7 +144,8 @@ require("lazy").setup({
     }
   },
   -- whitespaceを消してくれるやつ
-  {'cappyzawa/trim.nvim',
+  {
+    'cappyzawa/trim.nvim',
     config = function()
       require('plugins.trim')
     end
