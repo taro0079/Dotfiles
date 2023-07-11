d8d69f6459c4ca36446362cffb0799aec69c1fdb
Taro Morita <morita0079@gmail.com>
2023-07-12 00:25:34 +0900

    delete packer.nvim

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# Install packer for neovim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
